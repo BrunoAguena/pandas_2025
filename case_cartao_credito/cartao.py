2bd31b731292a2c2aae9451e2ce99491f69e4b98
Bruno Aguena <bruno.aguena@gmail.com>
2026-04-09 09:53:17 -0300

    agora sim final

--- case_cartao_credito/cartao.py
+++ case_cartao_credito/cartao.py
@@ -11,21 +11,26 @@ df["vlParcela"] = df["vlVenda"] / df["qtParcelas"]
 #%%
 df.head()
 # %%
-
-df["vlParcelaUnica"] = df.apply(lambda row:[row["vlParcela"]] * row["qtParcelas"], axis=1)
-df.explode("vlParcelaUnica")
+df["ordemParcela"] = df.apply(lambda row: [i for i in range(row["qtParcelas"])], axis=1)
+df
 
 # %%
 
+df_explode = df.explode("ordemParcela")
+df_explode
+# %%
 def calcDtParcela(row):
     dt = row ["dtTransacao"] + pd.DateOffset(months=row["ordemParcela"])
     dt = f"{dt.year}-{dt.month}"
     return dt
 
-df["ordemParcela"] = df.apply(calcDtParcela, axis=1)
-df_explode = df.explode("ordemParcela")
+df_explode["dtParcela"] = df_explode.apply(calcDtParcela, axis=1)
+df_explode
 
 # %%
 
-df_explode["dtParcela"] = df_explode.apply(lambda row: row["dtTransacao"] + pd.DateOffset(months=row["ordemParcela"]), axis=1)
-df_explode.head()
+(df_explode.groupby(["idCliente", "dtParcela"])
+            ["vlParcela"].sum()
+            .reset_index()
+            .pivot_table(index="idCliente", columns="dtParcela", values="vlParcela", fill_value=0)
+)
